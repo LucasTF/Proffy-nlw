aadfe8767c766e1410123515813fadd607093636
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-08-06 16:02:45 -0300

    Finalized GiveClasses container.

--- mobile/src/containers/GiveClasses/index.tsx
+++ mobile/src/containers/GiveClasses/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import GiveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 import * as Styled from './styles';
 
 const GiveClasses: React.FC = () => {
+	const navigation = useNavigation();
+
+	function navBackToHomeHandler() {
+		navigation.goBack();
+	}
+
 	return (
 		<Styled.Container>
-			<Text>Hello World</Text>
+			<Styled.Background resizeMode='contain' source={GiveClassesBgImage}>
+				<Styled.Title>Quer ser um Proffy?</Styled.Title>
+				<Styled.Description>
+					Para começar, você precisa se cadastrar como professor na
+					nossa plataforma web.
+				</Styled.Description>
+			</Styled.Background>
+
+			<Styled.Button onPress={navBackToHomeHandler}>
+				<Styled.ButtonText>Tudo bem</Styled.ButtonText>
+			</Styled.Button>
 		</Styled.Container>
 	);
 };
